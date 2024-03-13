4960499c006c099a70937ad001c207452046b65d
sunil <dev3sunil@gmail.com>
2024-03-13 11:09:09 +0530

    added restriction based on privileges

--- src/main/java/com/bizzlog/gateway/dto/OrgFeatureFlagsDTO.java
+++ src/main/java/com/bizzlog/gateway/dto/OrgFeatureFlagsDTO.java
@@ -3,15 +3,10 @@ package com.bizzlog.gateway.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
-public class Privilege {
-
-    private long id;
-
-    private String privilege;
+public class OrgFeatureFlagsDTO {
+    private String feature;
+    private Boolean enabled;
 }
